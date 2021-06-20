a7f456cad4648c87e2b70d0cb793a16b0ea13b28
kthatoto <kthatoto@gmail.com>
2021-06-21 01:55:39 +0900

    Display title with composition api

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="main">
+    <h1>{{ title }}</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from '@vue/composition-api'
 
 export default defineComponent({
+  setup () {
+    const title = ref('Title')
+    return {
+      title
+    }
+  }
 })
 </script>
